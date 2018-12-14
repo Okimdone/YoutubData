9d7e3ab7c7a98fdbbf2f8bd76c1b67e8b0b39928
Okimdone <dante-street-fighter@outlook.fr>
2018-12-14 03:37:21 +0100

    Added more (documentation), A function that gets comments information from a given parentid, some code into the main method, and a note so bad output

--- youtube-api/src/main/java/Quickstart.java
+++ youtube-api/src/main/java/Quickstart.java
@@ -10,6 +10,7 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.client.util.DateTime;//For setPublishedBefore/From(DateTime x)
+import com.google.api.client.util.ArrayMap;
 
 import com.google.api.services.youtube.YouTubeScopes;
 import com.google.api.services.youtube.model.*;
@@ -23,7 +24,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;//search video.list
 import java.util.Date;
-import java.util.HashMap;   //search video.list
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -99,12 +100,22 @@ public class Quickstart {
                 .build();
     }
 
+    /** Main program, searched for a (query + date interval) return (videoid, channelid) as a tuple
+     *  searches for the videoIds and stores the found information in the List<Video> videoItems
+     *  searches for the channelIds and stores the found infomation in the List<Channel> channelItems
+     *  searches for the CommentThreads for the given videoIds and stores the data in the List<CommentThread> CommentThreadItems
+     *  searches every Comment in the CommentThreads for comments-replies and stores all found comments into List<Comment> CommentItems 
+     *  searches every commentItem for a channelId and searches for that channelId s data and adds it to List<Channel> channelItems  
+     */
     public static void main(String[] args) {
         try {
+            // Youtube s magic
             youtube = getYouTubeService();
-            List<SearchResult> items = getVideosByQuery("hello"
-                                                    , new Date(/*year*/ 2018-1900,/*month*/ 10 + 1,/*Day*/ 12)
-                                                    , new Date(/*year*/ 2018-1900,/*month*/ 11 + 1 ,/*Day*/ 12)
+            
+            // Search for <query>
+            List<SearchResult> items = getVideosByQuery("                                                                                                                                                                                                                                                                                                                                                           hello"
+                                                    , new Date(/*year*/ 2018-1900,/*month*/ 11 - 1,/*Day*/ 12)
+                                                    , new Date(/*year*/ 2018-1900,/*month*/ 12 - 1 ,/*Day*/ 12)
                                                     );
             if(items != null){
                 String videoIds = null;
@@ -114,41 +125,83 @@ public class Quickstart {
                 List<Channel> channelItems = new ArrayList<>(); 
                 // A list of CommentThreads informations for everyvideo in the videoItems
                 List<CommentThread> commentThreadItems = new ArrayList<>();
+                // A list of Comments informations for everyvideo in the CommentThreads
+                List<Comment> commentItems = new ArrayList<>();
                 
                 // Collectusefull information in the searched items (videoIds and channelIds)
-                for(SearchResult item : items){
-                    if(videoIds == null){
-                        videoIds = item.getId().getVideoId();
-                    } else {
-                        videoIds += "," + item.getId().getVideoId();
-                    }
-                    channelItems.add(getChannelItem(item.getSnippet().getChannelId()));
-                    try {
-                        List<CommentThread> cTs = getCommentThreadsByVId("6r5y4bsVUt8"/*item.getId().getVideoId()*/); 
-                        if(cTs != null){
+                // For Every item return by our search
+                for (SearchResult item : items) {
+                    
+                    // Append the [videoIds,...] string for a multiple searchByVideoIds search
+                    if(videoIds == null)    videoIds = item.getId().getVideoId();
+                    else                    videoIds += "," + item.getId().getVideoId();
+                    
+                    // Search for ChannelIds and add returned items into the <channelItems> : List
+                    channelItems.add(  getChannelItem( item.getSnippet().getChannelId() )  );
+                    
+                    // Try-Catch for 403 Forbidden recovery of the CommentThread items (comments for a given video might be disabled)
+                    try { 
+                        // Collect CommentThreads for each and every video(id), found by the given seach (if there is any) 
+                        List<CommentThread> cTs = getCommentThreadsByVId(item.getId().getVideoId()); 
+
+                        // If an the CommentThread list is non empty
+                        if (cTs != null) {
+                            
+                            // Then Append every Item 
                             for(CommentThread cT : cTs){
-                                System.out.println(cT.getSnippet().getTopLevelComment().getSnippet().getTextDisplay());
-                                System.out.println();
-                                System.out.println(cT.getSnippet().getTopLevelComment().getSnippet().getTextOriginal());
-                                System.out.println();
-                                System.out.println();
-                                // the Making of the commentTread List of items
+                                // Add any found commentThreads into the commentThreadItems list 
                                 commentThreadItems.add(cT);
+
+                                // Add the topLevelComments from the each commentThread item to the Commentitems List
+                                Comment commentOfCT =  cT.getSnippet().getTopLevelComment();
+                                
+                                // Add the top level comment of the current thread into the Comments list
+                                commentItems.add(commentOfCT);
+                                // Add its Author s channel s data into the Channels list
+                                channelItems.add(  getChannelItem( ((ArrayMap<String,String>)commentOfCT.getSnippet().getAuthorChannelId()).get("value") )  );
+
+                                // Get a list of the Comments replying on the <commentOfCT>  
+                                List<Comment> commentsChildren = getCommentsByParentId( commentOfCT.getId(), cT.getSnippet().getTotalReplyCount() );
+
+                                // Add every Comment and its author s channel into the recovered list into The <CommentItems> list
+                                for(Comment comment : commentsChildren){
+                                    commentItems.add(comment);
+                                    channelItems.add(getChannelItem( ((ArrayMap<String,String>)comment.getSnippet().getAuthorChannelId()).get("value")));    
+                                }
                             }
                         }
-                    } catch (GoogleJsonResponseException e){
+                    } catch (GoogleJsonResponseException e){}
 
+                }
+
+                // seach and store Videoitems s data
+                videoItems = getVideoItems(videoIds); 
+
+                // Print the results out
+
+                System.out.println("Listing Videos s data :");                
+                if(commentItems != null){
+                    for(Video video : videoItems){    
+                        System.out.println("Video :   " + video.getSnippet().getTitle());
+                        System.out.println("Channel : " + video.getSnippet().getChannelTitle() + "\n");
                     }
-                }videoItems = getVideoItems(videoIds);
-                for(Video v : videoItems){
-                    System.out.println( "Title : " + v.getSnippet().getLocalized().getTitle() + "\nChannel :  " +
-                                        v.getSnippet().getChannelTitle() + "\n\n"
-                                        );
-                    //System.out.println(channelItems);
-                } 
-                /*System.out.println();
-                System.out.println(getChannelItem(commentThreadItems.toString()));
-                */
+                }else System.out.println("No videos");
+                
+                if(commentItems != null){
+                    System.out.println("Listing Channels :");
+                    for(Channel channel : channelItems){    
+                        System.out.println("Channel : " + channel.getSnippet().getTitle() + "\n");
+                    }
+                }else System.out.println("No Channels");
+                
+                System.out.println("Listing Comments : ");
+                if(commentItems != null){
+                    for(Comment comment : commentItems){    
+                        System.out.println("Author : " + comment.getSnippet().getAuthorDisplayName());
+                        System.out.println("text : " +   comment.getSnippet().getTextOriginal());
+                    } 
+                }else System.out.println("No comments");
+                
             }
         } catch (GoogleJsonResponseException e) {
             e.printStackTrace();
@@ -169,17 +222,17 @@ public class Quickstart {
     private static List<SearchResult> getVideosByQuery(String query, Date dateFrom, Date dateTo)
     throws IOException
     {
-            YouTube.Search.List searchListByKeywordRequest = youtube.search().list("id,snippet");
-            //to change (it s set to 5 by default)
-            searchListByKeywordRequest.setMaxResults(Long.parseLong("1"));
-            searchListByKeywordRequest.setType("video");
-            if (query != "")    searchListByKeywordRequest.setQ(query);
-            searchListByKeywordRequest.setPublishedAfter(new DateTime(dateFrom));
-            searchListByKeywordRequest.setPublishedBefore(new DateTime(dateTo ));
-            // Possible Values (date, rating , relevance, title, videoCount, viewCount)
-            searchListByKeywordRequest.setOrder("date");//"relevance" by default.
+        YouTube.Search.List searchListByKeywordRequest = youtube.search().list("id,snippet");
+        //to change (it s set to 5 by default)
+        searchListByKeywordRequest.setMaxResults(Long.parseLong("2"));
+        searchListByKeywordRequest.setType("video");
+        if (query != "")    searchListByKeywordRequest.setQ(query);
+        searchListByKeywordRequest.setPublishedAfter(new DateTime(dateFrom));
+        searchListByKeywordRequest.setPublishedBefore(new DateTime(dateTo ));
+        // Possible Values (date, rating , relevance, title, videoCount, viewCount)
+        //searchListByKeywordRequest.setOrder("date");//"relevance" by default.
 
-            return (searchListByKeywordRequest.execute().getItems());  
+        return (searchListByKeywordRequest.execute().getItems());  
     }      
 
     /** Takes a list of ids as a comma separated ids values and return an array of video*/
@@ -203,6 +256,17 @@ public class Quickstart {
     throws IOException {
         YouTube.CommentThreads.List commentThreadsListByVideoIdRequest = youtube.commentThreads().list("snippet,replies");
         if (videoId != "")  commentThreadsListByVideoIdRequest.setVideoId(videoId);
+        commentThreadsListByVideoIdRequest.setOrder ("relevance");
+        commentThreadsListByVideoIdRequest.setMaxResults(Long.parseLong("1"));
         return commentThreadsListByVideoIdRequest.execute().getItems();
+    }
+
+    /** get the comments items by the given comment parent_id */
+    private static List<Comment> getCommentsByParentId(String parentId, Long replyNumber)
+    throws IOException {
+        YouTube.Comments.List commentsListRequest = youtube.comments().list("snippet");
+        if (parentId != "") commentsListRequest.setParentId( parentId );
+        commentsListRequest.setMaxResults( replyNumber );
+        return commentsListRequest.execute().getItems();
     }
 }
