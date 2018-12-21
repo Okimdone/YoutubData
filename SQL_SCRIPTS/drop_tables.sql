f4c977822c89ac67b18b22810da2a857dd289d66
Okimdone <dante-street-fighter@outlook.fr>
2018-12-21 06:34:49 +0000

    New dd (#3)
    * a better simplified DD
    
    * Added Mcd based on the BIG_DDS.csv file
    
    * Added a RELATIONAL MLD for the datebase
    
    * Added sql.txt via upload 
    
    * Added SQL SCRIPT Files for creating and deleting the youtube database

---
+++ SQL_SCRIPTS/drop_tables.sql
@@ -0,0 +1,19 @@
+ALTER TABLE Query_plage DISABLE CONSTRAINT qry_plge_qry_id_FK;
+ALTER TABLE Video DISABLE CONSTRAINT v_ch_id_FK;
+ALTER TABLE Trouver DISABLE CONSTRAINT Tr_v_id_FK;
+ALTER TABLE Trouver DISABLE CONSTRAINT Tr_qry_plage_id_FK;
+ALTER TABLE Comments DISABLE CONSTRAINT co_ch_id_FK;
+ALTER TABLE Comments_video DISABLE CONSTRAINT cv_co_id_FK;
+ALTER TABLE Comments_video DISABLE CONSTRAINT cv_v_id_FK;
+ALTER TABLE Comments_reponse DISABLE CONSTRAINT cr_co_id_FK;
+ALTER TABLE Comments_reponse DISABLE CONSTRAINT cr_co_p_id_FK;
+
+
+drop table Comments_reponse;
+drop table Comments_video;
+drop table comments;
+drop table trouver;
+drop table video;
+drop table channel;
+drop table Query_plage;
+drop table query;
